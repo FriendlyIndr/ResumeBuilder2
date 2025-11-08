367fb50999bd62306e8f371969cd00269027d2e0
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2025-11-08 20:07:57 +0530

    CORS enabled.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users" , userRoutes);
