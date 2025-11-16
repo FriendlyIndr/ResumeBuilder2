7d3e3eaafc5f8b1449d995587a399d59a981e86b
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2025-11-16 15:33:50 +0530

    Templates section update.

--- frontend/src/components/Templates.jsx
+++ frontend/src/components/Templates.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const Templates = () => {
+  const templates = [
+    { name: "Sales" },
+    { name: "Electrical Engineering" },
+    { name: "Computer Science" },
+  ]
+
   return (
-    <div>Templates</div>
+    <div>
+      <div>
+        <h1 className='text-lg mt-6'>Choose a template</h1>
+
+        <div className='grid grid-cols-3 gap-4'>
+          {templates.map((template, index) => (
+            <div
+              key={index}
+              className='bg-slate-200 rounded-lg p-2'
+            >
+              <img />
+              {template.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
